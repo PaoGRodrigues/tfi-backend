e2e1b83d12cc9a749df6a89d5b535ac953de3c7d
PaoRodrigues <paogrodrigues@gmail.com>
2022-05-02 07:43:13 -0300

    Add get active flows return nil first and then return successfully successful test

--- app/traffic/usecase/traffic_usecase_bytes_per_dst_test.go
+++ app/traffic/usecase/traffic_usecase_bytes_per_dst_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
-func TestGetBytesPerDestReturnsBytes(t *testing.T) {
+func TestGetBytesPerDestReturnsBytesSuccessfully(t *testing.T) {
 
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
@@ -46,6 +46,56 @@ func TestGetBytesPerDestReturnsBytes(t *testing.T) {
 
 	mockSearcher := mocks.NewMockTrafficUseCase(ctrl)
 	mockSearcher.EXPECT().GetActiveFlows().Return(expectedFlowFromSearcher)
+
+	parser := usecase.NewBytesDestinationParser(mockSearcher)
+	got, err := parser.GetBytesPerDestination()
+
+	if err != nil {
+		t.Fail()
+	}
+
+	if !reflect.DeepEqual(expected, got) {
+		t.Errorf("expected:\n%+v\ngot:\n%+v", expected, got)
+	}
+}
+
+func TestGetBytesPerDestSearcherActiveFlowsIsEmptyReturnsBytesSuccessfully(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	expectedFlowFromSearcher := []domains.ActiveFlow{
+		domains.ActiveFlow{
+			Client: domains.Client{
+				Name: "Local",
+				Port: 12345,
+				IP:   "192.168.4.1",
+			},
+			Server: domains.Server{
+				IP:                "8.8.8.8",
+				IsBroadcastDomain: false,
+				IsDHCP:            false,
+				Port:              443,
+				Name:              "google.com.ar",
+			},
+			Protocol: domains.Protocol{
+				L4: "TCP",
+				L7: "TLS.Google",
+			},
+			Bytes: 5566778,
+		},
+	}
+
+	expected := []usecase.BytesPerDestination{
+		usecase.BytesPerDestination{
+			Bytes:       expectedFlowFromSearcher[0].Bytes,
+			Destination: expectedFlowFromSearcher[0].Server.Name,
+		},
+	}
+
+	mockSearcher := mocks.NewMockTrafficUseCase(ctrl)
+	mockSearcher.EXPECT().GetActiveFlows().Return(nil)
+	mockSearcher.EXPECT().GetAllActiveTraffic().Return(expectedFlowFromSearcher, nil)
 
 	parser := usecase.NewBytesDestinationParser(mockSearcher)
 	got, err := parser.GetBytesPerDestination()
