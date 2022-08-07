4984c73154d591d3d4b8802f7e94bcdb39cdd964
PaoRodrigues <paogrodrigues@gmail.com>
2022-08-07 00:19:59 -0300

    Add successful test for getAllAlerts

---
+++ app/alerts/usecase/alert_usecase_searcher_test.go
@@ -0,0 +1,44 @@
+package usecase_test
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/PaoGRodrigues/tfi-backend/app/alerts/domains"
+	"github.com/PaoGRodrigues/tfi-backend/app/alerts/usecase"
+	mocks "github.com/PaoGRodrigues/tfi-backend/mocks/alerts"
+	"github.com/golang/mock/gomock"
+)
+
+func TestGetAllAlertsReturnListOfAlerts(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	expected := []domains.Alert{
+		domains.Alert{
+			Name:      "test",
+			Subtype:   "network",
+			Family:    "network",
+			Timestamp: time.Time{},
+			Score:     "1",
+			Severity:  "2",
+			Msg:       "testing Msg",
+		},
+	}
+
+	mockService := mocks.NewMockAlertService(ctrl)
+	mockService.EXPECT().GetAllHosts().Return(expected, nil)
+
+	alertSearcher := usecase.NewAlertSearcher(mockService)
+	got, err := alertSearcher.GetAllAlerts()
+
+	if err != nil {
+		t.Fail()
+	}
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("expected:\n%+v\ngot:\n%+v", expected, got)
+	}
+}
