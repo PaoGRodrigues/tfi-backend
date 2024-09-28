20e1444b8ec28dcb13656399c089d4f2e4381240
PaoRodrigues <paogrodrigues@gmail.com>
2024-09-28 18:51:23 -0300

    Use two go rutines to enable all the checks

--- app/services/check_client.go
+++ app/services/check_client.go
@@ -3,9 +3,11 @@ package services
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
 )
 
 type HttpCheckResponse struct {
@@ -17,21 +19,60 @@ type Response struct {
 	Success bool
 }
 
-func (t *NtopNG) EnableChecks() {
-	for _, check := range Checks {
+/*
+for _, check := range Checks {
 		rsp, err := t.enableCheck(check)
 		if !rsp.Rsp.Success {
-			log.Fatalf("Error enabling check %s", err)
+			log.Fatalf("Error enabling check %s", check)
 		}
 		continue
 	}
+*/
+
+func (t *NtopNG) EnableChecks() {
+	checksChannel := make(chan Check)
+
+	// Put data into the initial channel
+	go func() {
+		for _, check := range Checks {
+			checksChannel <- check
+		}
+		close(checksChannel)
+	}()
+	fmt.Print(len(checksChannel))
+
+	// Declare the worker function which reads de checkChannel to make the request
+	worker := func() {
+		for check := range checksChannel {
+			rsp, err := t.enableCheck(check)
+			fmt.Printf("Enabling %s", check)
+			fmt.Print(rsp.Rsp.Success)
+			if err != nil {
+				log.Fatalf("Error enabling check %s", check)
+			}
+			if !rsp.Rsp.Success {
+				log.Fatalf("Error enabling check %s", check)
+			}
+		}
+	}
+
+	// WaitGroup is used for synchronous closing of the results channel when all work is done
+	numWorkers := 6
+	wg := &sync.WaitGroup{}
+	wg.Add(numWorkers)
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			worker()
+			wg.Done()
+		}()
+	}
 }
 
-func (t *NtopNG) enableCheck(currenrCheck Check) (HttpCheckResponse, error) {
+func (t *NtopNG) enableCheck(currentCheck Check) (HttpCheckResponse, error) {
 	client := &http.Client{}
 	endpoint := "/lua/rest/v2/enable/check.lua"
 
-	marshalled, err := json.Marshal(currenrCheck)
+	marshalled, err := json.Marshal(currentCheck)
 	if err != nil {
 		return HttpCheckResponse{}, err
 	}
