96e2bfab4ce16df465bb7fef15755d6657b2b989
PaoGRodrigues <paogrodrigues@gmail.com>
2025-04-29 15:46:44 -0300

    Add tests to ConfigureAlertsNotificationChannel

--- app/usecase/notificationchannel/configure_alerts_notification_channel_test.go
+++ app/usecase/notificationchannel/configure_alerts_notification_channel_test.go
@@ -1,6 +1,7 @@
 package notificationchannel_test
 
 import (
+	"fmt"
 	"testing"
 
 	usecase "github.com/PaoGRodrigues/tfi-backend/app/usecase/notificationchannel"
@@ -20,6 +21,49 @@ func TestConfigureChannelSuccessfully(t *testing.T) {
 	err := configureNotificationChannelUseCase.Configure("user", "token")
 
 	if err != nil {
+		t.Error("Testing error")
+	}
+}
+
+func TestConfigureChannelReturnErrorBecauseUserIsEmpty(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockChannel := notificationChannelPortsMock.NewMockNotificationChannel(ctrl)
+
+	configureNotificationChannelUseCase := usecase.NewConfigureChannelUseCase(mockChannel)
+	err := configureNotificationChannelUseCase.Configure("", "token")
+
+	if err == nil {
+		t.Error("Testing error")
+	}
+}
+
+func TestConfigureChannelReturnErrorBecauseTokenIsEmpty(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockChannel := notificationChannelPortsMock.NewMockNotificationChannel(ctrl)
+
+	configureNotificationChannelUseCase := usecase.NewConfigureChannelUseCase(mockChannel)
+	err := configureNotificationChannelUseCase.Configure("user", "")
+
+	if err == nil {
+		t.Error("Testing error")
+	}
+}
+
+func TestConfigureChannelReturnErrorBecauseChannelReturnError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockChannel := notificationChannelPortsMock.NewMockNotificationChannel(ctrl)
+	mockChannel.EXPECT().Configure("user", "token").Return(fmt.Errorf("Error test"))
+
+	configureNotificationChannelUseCase := usecase.NewConfigureChannelUseCase(mockChannel)
+	err := configureNotificationChannelUseCase.Configure("user", "token")
+
+	if err == nil {
 		t.Error("Testing error")
 	}
 }
